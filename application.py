7c6c9e3a9fd1fc7bcc411ad25fd3b9aa779dc0a5
James Waters <jwaters1993@outlook.com>
2022-07-13 20:43:40 -0400

    Added connection to AWS database. Put Nav Bar in seperate file added login page.

--- application.py
+++ application.py
@@ -1,5 +1,38 @@
 from email.mime import application
 from flask import Flask, render_template
+from config import config
+
+import psycopg2
+
+conn = None
+try:
+    # read connection parameters
+    params = config()
+
+    # connect to the PostgreSQL server
+    print('Connecting to the PostgreSQL database...')
+    conn = psycopg2.connect(**params)
+    
+    # create a cursor
+    cur = conn.cursor()
+    
+# execute a statement
+    print('PostgreSQL database version:')
+    cur.execute('SELECT version()')
+
+    # display the PostgreSQL database server version
+    db_version = cur.fetchall()
+    print(db_version)
+    
+# close the communication with the PostgreSQL
+    cur.close()
+except (Exception, psycopg2.DatabaseError) as error:
+    print(error)
+finally:
+    if conn is not None:
+        conn.close()
+        print('Database connection closed.')
+
 
 application = Flask(__name__)
 
@@ -10,3 +43,8 @@ def Hello():
 @application.route('/register')
 def register():
     return render_template("register.html")
+
+@application.route('/login')
+def login():
+    return render_template("login.html")
+    
